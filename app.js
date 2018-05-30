99cda7a2a7100b3f033962fafd0c58aefae3d70a
sciNtech <sntpresident@gmail.com>
2018-05-30 19:58:18 +0800

    Page update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = {
     projectId: "snt-june-camp",
     storageBucket: "",
     messagingSenderId: "304983406415"
-  };
+};
 firebase.initializeApp(config);
 var firebaseRef = firebase.database().ref();
 var currentTeam = "";
@@ -28,12 +28,6 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-function firebase_config() {
-    
-}
-
-// firebase_config()
-
 document.addEventListener('DOMContentLoaded', function() {
 
     console.log('App loaded')
@@ -41,8 +35,9 @@ document.addEventListener('DOMContentLoaded', function() {
     PlaygroundView = document.getElementById('playgroundPage');
     // PlaygroundView.style.display = "none";
 
-    var emailID = document.getElementById('email');
-    var password = document.getElementById('password');
+    // var emailID = document.getElementById('email');
+    // var password = document.getElementById('password');
+    var password = "Y4M10L";
     var team = document.getElementById('teamChoose');
     var loginBtn = document.getElementById('login');
 
@@ -51,10 +46,10 @@ document.addEventListener('DOMContentLoaded', function() {
             snapshot.forEach(function(child) {
                 currentTeamData.push(child.val())
             })
-            if (emailID.value != currentTeamData[2] || password.value != currentTeamData[3]) {
+            if (password != currentTeamData[2]) {
                 window.alert('Invalid credentials. Try again...')
                 password.value = ""
-            } else if (emailID.value === currentTeamData[2] || password.value === currentTeamData[3]) {
+            } else if (password === currentTeamData[2]) {
                 LoginView.style.display = "none";
                 PlaygroundView.style.display = "block";
                 currentTeam = team.value;
@@ -71,13 +66,14 @@ function beginGames(team, teamData) {
     var gameCodeBoxText = document.getElementById('text');
     var gameCode = document.getElementById('gameCode');
     var confirmIDBtn = document.getElementById('confirmID');
+    var newStationGiver = document.getElementById('next-station-giver');
 
     gameCodeBox.style.visibility = "visible";
     confirmIDBtn.addEventListener('click', function() {
         firebaseRef.child("Game Codes").once("value", function(snapshot) {
             if (snapshot.val().includes(gameCode.value)) {
                 gameCodeBox.style.visibility = "hidden";
-                loadGame(gameCodeBox, team, gameCode.value);
+                loadGame(gameCodeBox, newStationGiver, gameCode, team, gameCode.value);
             } else {
                 gameCodeBoxText.innerText = "Please re-enter valid code..."
             }
@@ -85,8 +81,9 @@ function beginGames(team, teamData) {
     })
 }
 
-function loadGame(box, team, GAMECODE) {
-    console.log('Loading Game...')
+function loadGame(box, boxTextNode, codeInput, team, GAMECODE) {
+    var correct = false;
+    console.log('Loading Game for ' + team)
     var playground = document.getElementById('playground')
     var pgQuestion = document.getElementById('pg-question')
     var pgImage = document.getElementById('pg-image')
@@ -108,60 +105,123 @@ function loadGame(box, team, GAMECODE) {
         pgOption3.innerText = "3. " + snapshot.val()['Options']['Option 3']
         pgOption4.innerText = "4. " + snapshot.val()['Options']['Option 4']
         if (GAMECODE == '06IQH') {
-            pgImage.src = 'resistance_chart.jpg'
+            pgImage.src = 'Resistance.jpg'
+            pgImage.style.width = "300px";
         } else if (GAMECODE == 'P3S7R') {
             pgImage.src = 'capacitor.jpg'
+            pgImage.style.width = "225px";
         } else if (GAMECODE == 'XAMIK') {
             pgImage.src = 'transistor.jpg'
+            pgImage.style.width = "150px";
         } else {
             pgImage.style.display = 'none';
         }
 
         submitBtn.addEventListener('click', function() {
-            if (answerBox.value == snapshot.val()['Correct']) {
+            console.log("User answer: " + answerBox.value)
+            // console.log("actual: " + snapshot.val()['Correct'] + " user: " + answerBox.value)
+            if (snapshot.val()['Correct'] === answerBox.value) { // Correct answer
+                answerBox.addEventListener('change', function() {
+                    console.log("changed to: " + answerBox.value);
+                })
                 console.log('Correct')
-                // Give next location
-                firebaseRef.child("Route").once("value", function(routeshot) {
-                    firebaseRef.child("Teams").child(team).child("GameIndex").once("value").then(function(indexshot) {
-                        if (indexshot.val() == 10) {
-                            window.alert("Congratulations, You have finished all 10 questions! You may now return to your starting location.")
+                correct = true;
+                firebaseRef.child("Route").once("value").then(function(routeshot) { // Getting route list
+                    firebaseRef.child("Teams").child(team).once("value").then(function(teamShot) { // Getting next route index
+                        if (teamShot.val()['GameIndex'] == 10) {
+                            window.alert("You have completed the quiz! You may now quit the app and move on to the next activity!")
                             location.reload()
                         } else {
-                            window.alert('The next Gamecode is at this location: ' + routeshot.val()[indexshot.val()])
-                            firebaseRef.child("Teams").child(team).child("GameIndex").set(indexshot.val() + 1)
+                            var newGame = routeshot.val()[teamShot.val()['GameIndex']]
+                            boxTextNode.innerText = "Next Station:\n" + newGame;
+                            firebaseRef.child("Teams").child(team).child("GameIndex").set(teamShot.val()['GameIndex'] + 1)
+                            firebaseRef.child("Teams").child(team).child("Flex Bucks").set(teamShot.val()['Flex Bucks'] + 10)
                         }
                     })
-                    firebaseRef.child("Teams").child(team).child("Flex Bucks").once("value", function(FBshot) {
-                        var currentFB = FBshot.val()
-                        firebaseRef.child("Teams").child(team).child("Flex Bucks").set(currentFB + 10);
-                    })
                 })
-                answerBox.value = ''
-                playground.style.visibility = "hidden";
+                playground.style.visibility = "hidden"; // Move back to GAMECODE screen
                 box.style.visibility = "visible";
+                codeInput.value = "";                
+
             } else {
                 console.log('Incorrect')
-                // Give next location
-                firebaseRef.child("Route").once("value", function(routeshot) {
-                    firebaseRef.child("Teams").child(team).child("GameIndex").once("value").then(function(indexshot) {
-                        if (indexshot.val() == 10) {
-                            window.alert("Congratulations, You have finished all 10 questions! You may now return to your starting location.")
+                correct = false;
+                firebaseRef.child("Route").once("value").then(function(routeshot) { // Getting route list
+                    firebaseRef.child("Teams").child(team).once("value").then(function(teamShot) { // Getting next route index
+                        if (teamShot.val()['GameIndex'] == 10) {
+                            window.alert("You have completed the quiz! You may now quit the app and move on to the next activity!")
                             location.reload()
                         } else {
-                            window.alert('The next Gamecode is at this location: ' + routeshot.val()[indexshot.val()])
-                            firebaseRef.child("Teams").child(team).child("GameIndex").set(indexshot.val() + 1)
+                            var newGame = routeshot.val()[teamShot.val()['GameIndex']]
+                            boxTextNode.innerText = "Next Station:\n" + newGame;
+                            firebaseRef.child("Teams").child(team).child("GameIndex").set(teamShot.val()['GameIndex'] + 1)
+                            firebaseRef.child("Teams").child(team).child("Flex Bucks").set(teamShot.val()['Flex Bucks'])
                         }
                     })
-                    firebaseRef.child("Teams").child(team).child("Flex Bucks").once("value", function(FBshot) {
-                        var currentFB = FBshot.val()
-                        firebaseRef.child("Teams").child(team).child("Flex Bucks").set(currentFB);
-                    })
                 })
-                answerBox.value = ''
-                playground.style.visibility = "hidden";
+                playground.style.visibility = "hidden"; // Move back to GAMECODE screen
                 box.style.visibility = "visible";
+                codeInput.value = "";
             }
         })
 
+        // submitBtn.addEventListener('click', function() {
+        //     if ((answerBox.value) === (snapshot.val()['Correct'])) {
+        //         console.log('Correct | Your answer: ' + answerBox.value + " | Correct: " + snapshot.val()['Correct'])
+        //         // answerBox.value = ''
+        //         // Give next location
+        //         firebaseRef.child("Route").once("value", function(routeshot) {
+        //             firebaseRef.child("Teams").child(team).child("GameIndex").once("value").then(function(indexshot) {
+        //                 if (indexshot.val() == 10) {
+        //                     window.alert("Congratulations, You have finished all 10 questions! You may now exit the game and return to your starting location.")
+        //                     location.reload()
+        //                 } else {
+        //                     // window.alert('The next Gamecode is at this location: ' + routeshot.val()[indexshot.val()])
+        //                     boxTextNode.innerText = "Next Station:\n" + routeshot.val()[indexshot.val()]
+        //                     firebaseRef.child("Teams").child(team).child("GameIndex").set(indexshot.val() + 1)
+        //                     firebaseRef.child("Teams").child(team).child("Flex Bucks").once("value", function(FBshot) {
+        //                         var currentFB = FBshot.val()
+        //                         console.log('Old: ' + currentFB + " | New: " + FBshot.val() + 10)
+        //                         firebaseRef.child("Teams").child(team).child("Flex Bucks").set(FBshot.val() + 10);
+        //                     })
+        //                 }
+        //             })
+        //         })
+        //         playground.style.visibility = "hidden";
+        //         box.style.visibility = "visible";
+        //         codeInput.value = "";
+
+        //     } else {
+        //         console.log('Incorrect | Your answer: ' + answerBox.value + " | Correct: " + snapshot.val()['Correct'])
+        //         // answerBox.value = ''
+        //         // Give next location
+        //         firebaseRef.child("Route").once("value", function(routeshot) {
+        //             firebaseRef.child("Teams").child(team).child("GameIndex").once("value").then(function(indexshot) {
+        //                 if (indexshot.val() == 10) {
+        //                     window.alert("Congratulations, You have finished all 10 questions! You may now exit the game and return to your starting location.")
+        //                     location.reload()
+        //                 } else {
+        //                     // window.alert('The next Gamecode is at this location: ' + routeshot.val()[indexshot.val()])
+        //                     boxTextNode.innerText = "Next Station:\n" + routeshot.val()[indexshot.val()]
+        //                     firebaseRef.child("Teams").child(team).child("GameIndex").set(indexshot.val() + 1)
+        //                     firebaseRef.child("Teams").child(team).child("Flex Bucks").once("value", function(FBshot) {
+        //                         var currentFB = FBshot.val()
+        //                         console.log('Old: ' + currentFB + " | New: " + FBshot.val() - 10)
+        //                         firebaseRef.child("Teams").child(team).child("Flex Bucks").set(FBshot.val());
+        //                     })
+        //                 }
+        //             })
+        //         })
+        //         // answerBox.value = ''
+        //         playground.style.visibility = "hidden";
+        //         box.style.visibility = "visible";
+        //         codeInput.value = "";
+        //     }
+        // })
+
     })
+
+    function getAnswer(userAnswer, GAMECODE) {
+        firebaseRef.child("")
+    }
 }
